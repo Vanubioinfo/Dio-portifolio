1678a39523fae27fcd9cedfb2386c1eb5282eb26
vanubioinfo <vanubio06@gmail.com>
2022-10-04 23:40:36 -0300

    up api

--- javascript-portfolio/assets/js/api.js
+++ javascript-portfolio/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Vanubioinfo/portifolio-js/main/javascript-portfolio/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Vanubioinfo/Dio-portifolio/master/javascript-portfolio/data/profile.json';
 
     const response = await fetch(url)
     const profileData = await response.json()
